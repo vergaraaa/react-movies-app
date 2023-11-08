8285fe7853608d9b4a4364f2bf109592e3b28c5a
Edgar Vergara <edgarvm13@hotmail.com>
2023-11-08 05:05:05 +0100

    feat: get data from AddMovie form

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,12 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const AddMovie = () => {
+
+    const titleComponent = "Add movie";
+
+    const [movieState, setMovieState] = useState({
+        title: "",
+        description: "",
+    });
+
+    const { title, description } = movieState;
+
+    const getFormData = e => {
+        e.preventDefault();
+
+        // get data from form
+        let target = e.target;
+        let title = target.title.value;
+        let description = target.description.value;
+
+        // create movie object to save
+        let movie = {
+            id: new Date().getTime(),
+            title,
+            description
+        }
+
+        setMovieState(movie);
+    }
+
   return (
     <div className="add">
-        <h3 className="title">Add movies</h3>
-        <form>
-            <input type="text" placeholder="Title" />
-            <textarea placeholder="Description"></textarea>
+        <h3 className="title">{titleComponent}</h3>
+
+        <strong>
+            {(title && description) && `You have created the movie ${title}`}
+        </strong>
+
+        <form onSubmit={getFormData}>
+            <input 
+                type="text" 
+                id='title' 
+                name='title'
+                placeholder="Title" 
+            />
+            <textarea 
+                id='description'
+                name='description'
+                placeholder="Description"
+            ></textarea>
+
             <input type="submit" value="Save" />
         </form>
     </div>
