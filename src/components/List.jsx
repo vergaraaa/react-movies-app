bd396bbf83ebc1d907f81cb17aa32934d74a684e
Edgar Vergara <edgarvm13@hotmail.com>
2023-11-09 20:15:23 +0100

    feat: display movies from localStorage

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,39 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const List = () => {
+
+  const [movies, setMovies] = useState([]);
+  
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  const getMovies = () => {
+    let movies = JSON.parse(localStorage.getItem("movies"));
+
+    setMovies(movies);
+  }
+
   return (
     <>
-        <article className="movie-item">
-                <h3 className="title">Web development</h3>
-                <p className="description">github.com/vergaraaa</p>
+    {
+      movies != null 
+        ? movies.map(movie => {
+            return (
+              <article key={movie.id} className="movie-item">
+                <h3 className="title">{movie.title}</h3>
+                <p className="description">{movie.description}</p>
 
                 <button className="edit">Edit</button>
                 <button className="delete">Delete</button>
-            </article>
-
-            <article className="movie-item">
-                <h3 className="title">Web development</h3>
-                <p className="description">github.com/vergaraaa</p>
-
-                <button className="edit">Edit</button>
-                <button className="delete">Delete</button>
-            </article>
-
-            <article className="movie-item">
-                <h3 className="title">Web development</h3>
-                <p className="description">github.com/vergaraaa</p>
-
-                <button className="edit">Edit</button>
-                <button className="delete">Delete</button>
-            </article>
-
-            <article className="movie-item">
-                <h3 className="title">Web development</h3>
-                <p className="description">github.com/vergaraaa</p>
-
-                <button className="edit">Edit</button>
-                <button className="delete">Delete</button>
-            </article>
+              </article>
+            );
+          })
+      : <h2>
+          No movies to show
+        </h2>
+    }
     </>
   )
 }
